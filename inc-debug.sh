c41083031db0c5435b284561ef63caf9398aed74
aguy tech <aguytech@free.fr>
2021-12-08 20:42:59 +0100

    lxd-alpine:global rubish :o)

--- inc-debug.sh
+++ inc-debug.sh
@@ -32,7 +32,35 @@ _lxc_exec_e() { local ct=$1 && shift; lxc exec ${ct} -- sh -c "$*" || echo "Unab
 _var_replace_vars() {
     local vars
 
- 
+        case $1 in
+            apache)
+                vars="S_VM_PATH_SHARE S_RSYSLOG_PTC S_RSYSLOG_PORT _DOM_FQDN _IPTHIS _IPS_AUTH _APA_PATH_WWW _APA_ADMIN _APA_SUB _CIDR_VM" ;;
+            fail2ban)
+                vars="S_DOM_FQDN S_EMAIL_TECH S_HOST_PATH_LOG _IPS_IGNORED _SSH_PORT" ;;
+            haproxy)
+                vars="S_RSYSLOG_PORT S_SERVICE[mail] S_EMAIL_ADMIN _SOMAXCONN _HPX_PATH_SSL _HPX_CERTBOT_PORT _HPX_STATS_PORT _HPX_STATS_2_PORT S_DOM_FQDN S_DOM_NAME _HPX_CT_NAME _HPX_ACCESS_USER _HPX_ACCESS_PWD _HPX_ACCESS_URI _HPX_DNS_DEFAULT _SERVER_DEFAULT" ;;
+            logrotate)
+                vars="S_PATH_LOG S_HOST_PATH_LOG S_VM_PATH_LOG S_PATH_LOG_INSTALL S_PATH_LOG_SERVER" ;;
+            mail)
+                vars="S_SERVICE[mail] S_PATH_CONF S_DOM_FQDN S_EMAIL_TECH S_EMAIL_ADMIN _MEL_PATH_SQL _MEL_PATH_SSL _MEL_PATH_VMAIL _MEL_PATH_LOCAL _MEL_PATH_SIEVE _MEL_DB_HOST _MEL_DB_NAME _MEL_DB_USER _MEL_DB_PWD _MEL_SSL_SCHEME _MEL_DB_PFA_USER _MEL_DB_PFA_PWD _MEL_CIDR _IPS_CLUSTER _MEL_VMAIL_USER _MEL_VMAIL_UID" ;;
+            rspamd)
+                vars="S_RSPAMD_PORT[proxy] S_RSPAMD_PORT[normal] S_RSPAMD_PORT[controller] S_CACHE_PORT S_CACHE_READ_PORT _MEL_CTS_RDS _IPS_CLUSTER _MEL_CIDR _MEL_PATH_SPAM _MEL_PATH_DKIM" ;;
+            mariadb)
+                vars="S_DB_MARIA_PORT _MDB_VM_PATH _MDB_PATH_BINLOG _MDB_PATH_LOG _MDB_MAX_BIN_SIZE _MDB_EXPIRE_LOGS_DAYS _MDB_MASTER_ID _MDB_SLAVE_ID _MDB_REPLICATE_EXCEPT _MDB_REPLICATE_ONLY" ;;
+            php)
+                vars="_PHP_SERVICE _PHP_FPM_SOCK _PHP_FPM_ADMIN_SOCK _IP_HOST_VM" ;;
+            pma)
+                vars="_APP_URI _PMA_HOST _APP_DB_PORT _APP_DB_USER _APP_DB_PWD _APP_BLOWFISH _APP_PATH_UP _APP_PATH_DW" ;;
+            redis)
+                vars="S_PATH_LOG S_CACHE_PORT S_CACHE_READ_PORT _RDS_PATH_LIB _RDS_CT_NAME _RDS_QUORUM _RDS_SYNC _RDS_DOWN _RDS_FAILOVER _RDS_BACKLOG _RDS_TIMEOUT _RDS_KEEPALIVE _RDS_MEM_MAX _RDS_MEM_POL _RDS_DATABASES _RDS_LOGLEVEL _RDS_FILE_SENTINEL _RDS_PROXY" ;;
+            rsyslog)
+                vars="S_SERVICE[log] S_SERVICE[mail] S_PATH_LOG S_HOST_PATH_LOG S_VM_PATH_LOG S_RSYSLOG_PORT S_RSYSLOG_PTC" ;;
+            script)
+                vars="S_PATH_SCRIPT" ;;
+            *)
+                _exite "${FUNCNAME} Group: '${opt}' are not implemented yet" ;;
+        esac
+
     echo ${vars}
 }
 _var_replace() {
@@ -84,10 +112,11 @@ alias _evalq=eval
 alias _evalr="eval sudo"
 alias _evalrq="eval sudo"
 
+alias _confset=echo
+
 . /server/server.conf
 . /server/install.conf
 . /server/lxd.conf
-. /server/mail-ambau.ovh.lxd.conf
 _DATE=`date "+%Y%m%d"`
 _SDATE=`date +%s`
 _CIDR_VM=`sed -n 's|.* s_cidr=\([^ ]*\).*|\1|p' <<<${S_HOST_VM_ETH[default]}`
